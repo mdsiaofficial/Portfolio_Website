9a3f0a114b50b3f0bbf9bd7a79653b4c5391278a
mdsiaofficial <mdsiaofficial@gmail.com>
2024-02-28 18:25:32 +0600

    new design following

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuToggler = document.querySelector("#menutoggler");
-const menuBar = document.querySelector("#menubar");
-
-
-menuToggler.addEventListener("click", ()=>{
-    menuBar.classList.toggle("hidden");
-});
